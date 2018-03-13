c867d6aea607f2795f7973c0ad4dd05881cafd57
Alexis Harper <37089946+Nihilistic-Furry@users.noreply.github.com>
2018-03-12 21:20:45 -0500

    Initial commit

---
+++ Leviathan/Main.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+
+constexpr std::chrono::nanoseconds timestep(16ms); //60 ticks per sec
+
+int main() {
+
+	//Init
+
+	auto startTime = std::chrono::high_resolution_clock::now(); //Get time
+
+	//Main loop
+	while (true) {
+
+		auto deltaTime = std::chrono::high_resolution_clock::now() - startTime; //Calculates delta (time since last frame)
+
+		//Limit FPS to 60 Hz
+		if (std::chrono::duration_cast<std::chrono::nanoseconds>(deltaTime) >= timestep) {
+
+			//Get inputs
+
+			//Update physics
+
+			//Update graphics
+
+		}
+
+	}
+
+	return 0;
+
+}
