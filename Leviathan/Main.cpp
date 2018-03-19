02833e6a6afc8f7f33dc30e26815c1e28d6d041a
Alexis Harper <37089946+Nihilistic-Furry@users.noreply.github.com>
2018-03-19 11:13:13 -0500

    Added GLEW & GLFW ode to Main.cpp
    Added so much code to get GLEW & GLFW to actually run and do their things.
    
    Mostly barebones things.
    
    And added stb_image.h (now I have far more lines). It's a single line image loading library that will come in handy later.

--- Leviathan/Main.cpp
+++ Leviathan/Main.cpp
@@ -13,15 +13,63 @@ using namespace std;
 
 constexpr std::chrono::nanoseconds timestep(16ms); //60 ticks per sec
 
-
 int main() {
 
 	//Init
 
+	//Initialize GLFW
+	if (!glfwInit()) {
+
+		cout << "[-] GLFW: GLFW failed to initialize";
+
+		return 1;
+
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); //Use OpenGL version 4.1
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //Maximum compatability
+
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //Make Mac OS happy, won't you?
+
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); //Window resizing will distort the image
+
+	GLFWwindow *window = glfwCreateWindow(640, 480, "Leviathan", NULL, NULL); //Create window
+
+	//Make sure window exists
+	if (!window) {
+
+		cout << "[-] GLFW: Failed to initialize window";
+
+		glfwTerminate();
+
+		return 1;
+
+	}
+
+	int frameWidth, frameHeight;
+	glfwGetFramebufferSize(window, &frameWidth, &frameHeight); //Gets virtual dimentions of window
+
+	glewExperimental = GL_TRUE; //The magic of GLEW
+
+	//Initialize GLEW
+	if (glewInit() != GLEW_OK) {
+
+		cout << "[-] GLEW: GLEW failed to initialize";
+
+		glfwTerminate();
+
+		return 1;
+
+	}
+
+	glViewport(0, 0, frameWidth, frameHeight); //This will make your life so much easier
+
 	auto startTime = std::chrono::high_resolution_clock::now(); //Get time
 
 	//Main loop
-	while (true) {
+	while (!glfwWindowShouldClose(window)) {
 
 		auto deltaTime = std::chrono::high_resolution_clock::now() - startTime; //Calculates delta (time since last frame)
 
@@ -30,15 +78,25 @@ int main() {
 
 			startTime = std::chrono::high_resolution_clock::now(); //Reset time every frame (necessary for propper timing)
 
+			glfwPollEvents(); //Checks for events such as glfwWindowShouldClose() or controller input
+
 			//Get inputs
 
 			//Update physics
 
 			//Update graphics
 
+			glClearColor(1.0f, 1.0f, 1.0f, 1.0f); //Black background
+
+			glClear(GL_COLOR_BUFFER_BIT); //Clears frame so next can render
+
+			glfwSwapBuffers(window); //Important
+
 		}
 
 	}
+
+	glfwTerminate(); 
 
 	return 0;
 
